b632b8cc1a227b0d2854f70e26a178f59438b89e
Nicolas Goy <kuon@goyman.com>
2012-10-16 14:47:25 +0200

    Initial Commit

---
+++ MapKit/MapKit.h
@@ -0,0 +1,13 @@
+//
+//  MapKit.h
+//  MapKit
+//
+//  Created by Nicolas Goy on 16.10.12.
+//  Copyright (c) 2012 Goyman SA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapKit : NSObject
+
+@end
---
+++ MapKit/MapKit.m
@@ -0,0 +1,13 @@
+//
+//  MapKit.m
+//  MapKit
+//
+//  Created by Nicolas Goy on 16.10.12.
+//  Copyright (c) 2012 Goyman SA. All rights reserved.
+//
+
+#import "MapKit.h"
+
+@implementation MapKit
+
+@end
